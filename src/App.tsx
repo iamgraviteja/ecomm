3a8daacd49dfab6d291b8eb5273c6df1f0f46f78
raviteja <raviteja>
2025-03-08 18:07:36 +0530

    Update build process and TypeScript configuration; remove unused App.css import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import "./App.css";
-
-import { Route,Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Navigation from "./components/navigation";
 import About from "./features/about";
--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router";
 
-import App from "./App.tsx";
-import { store } from "./store/index.ts";
+import App from "./App";
+import { store } from "./store/index";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
